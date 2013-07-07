6da12b6c42fcf0c79703350e1ba0efc2a223a368
Marcin Zawadzki <thelon78@gmail.com>
2013-07-07 12:19:13 +0200

    change

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var http = require('http');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
