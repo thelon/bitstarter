c80cba72df85636bc086eff03de9b62ad174d705
Marcin Zawadzki <thelon78@gmail.com>
2013-07-07 11:21:52 +0200

    change

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var file = fs.readFileSync('index.html');  
-	response.send(file);
+	response.send(file.toString('utf8'));
 	//response.send('Hello World2!');
 });
 
